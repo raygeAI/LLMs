d9ca98f219a3a0f8d74173d91ec15ebc8ed8b3ac
郭锐 <guorui@dossen.com>
2024-05-14 14:03:14 +0800

    ok

--- chatbi/config.py
+++ chatbi/config.py
@@ -2,7 +2,8 @@
 model_file = "../model/sqlcoder/sqlcode_7b_2"
 # 数据库元数据文件路径，主要由建表语句，表 join 关系描述构成
 metadata_file = "./prompt/metadata.sql"
-# simple_metadata_file = "./prompt/simple_metadata.sql"
+# 数据库元数据文件路径，主要由建表语句，表 join 关系描述构成
+simple_metadata_file = "./prompt/simple_metadata.sql"
 # 任务提示词
 prompts_file = "./prompt/prompt.md"
 # DuckDB 数据库文件位置
